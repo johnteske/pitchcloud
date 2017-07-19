30981d15925b536eb28e45a4aa395a0d7b106d22
John Teske <john.teske@gmail.com>
2017-07-18 22:01:34 -0700

    Add basic frequency control

--- _modules/cloud.js
+++ _modules/cloud.js
@@ -1,8 +1,14 @@
 PitchCloud.Cloud = (function() {
     function Cloud(args, out) {
+        var self = this;
+
         this.frequencies = args.frequencies;
-        this.size = this.frequencies.length;
+        this.size = this.frequencies.length; // TODO size based on overlaps, not freqs
         this.grainLength = args.grainLength || 4.0;
+
+        /**
+         * Initialize Grains
+         */
 
         this._grains = [];
 
@@ -11,6 +17,30 @@ PitchCloud.Cloud = (function() {
         }
 
         this._currentGrainIndex = 0;
+
+        /**
+         * Create controls
+         */
+
+        var controls = this.controls = {
+            container: document.createElement('div')
+        };
+
+        controls.frequencyInput = document.createElement('input');
+        controls.frequencyInput.value = this.frequencies;
+        controls.frequencyInput.addEventListener('change', function() {
+            var frequencies = this.value.split(',')
+                .map(function(f) {
+                    return +f;
+                });
+
+            self.frequencies = frequencies;
+            console.log(self.frequencies, frequencies);
+        });
+        controls.container.appendChild(controls.frequencyInput);
+
+        document.getElementsByClassName('clouds')[0]
+            .appendChild(controls.container);
     };
 
     Cloud.prototype._schedule = function(immediate) {
@@ -19,7 +49,7 @@ PitchCloud.Cloud = (function() {
             thisGrain = self._grains[self._currentGrainIndex];
 
         this._timeout = setTimeout(function() {
-            var randomIndex = Math.floor(Math.random() * self.size),
+            var randomIndex = Math.floor(Math.random() * self.frequencies.length),
                 frequency = self.frequencies[randomIndex];
 
             thisGrain.vco.setFrequency(frequency);
